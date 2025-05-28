91a9a6812518d3baa18d9831e91a1106d942d92c
ynosov <ynosov@provectus.com>
2025-05-28 16:13:34 -0500

    regexp 2

--- src/features/api/hotel-room.api.spec.ts
+++ src/features/api/hotel-room.api.spec.ts
@@ -8,7 +8,7 @@ import { AxiosResponse } from "axios";
 test.describe("@neevadmin @API Room Tests", () => {
   test(
     "User should get all rooms",
-    { tag: ["@smoke", "@regression", "@new"] },
+    { tag: ["@neevadmin", "@smoke", "@regression", "@new"] },
     async ({ roomApiHelper: roomApi, authHelper, registerCleanup }) => {
       let allRooms: Room[];
       let createdRoom1: Room, createdRoom2: Room, createdRoom3: Room;
